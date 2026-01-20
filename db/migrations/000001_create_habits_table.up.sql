67a5444de31e8ecd6bda43149679c9419f853ed6
Noel Vega <noelvegajr94@gmail.com>
2026-01-19 22:43:23 -0500

    unit of measurement col

--- db/migrations/000001_create_habits_table.up.sql
+++ db/migrations/000001_create_habits_table.up.sql
@@ -4,9 +4,11 @@ CREATE TABLE IF NOT EXISTS habits (
     description TEXT,
     completion_type VARCHAR(20) NOT NULL DEFAULT 'step',
     completions_per_day int NOT NULL DEFAULT 1,
+    unit_of_measurement VARCHAR(20) NOT NULL DEFAULT 'times'
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT chk_completion_type CHECK (completion_type IN ('step', 'custom')),
+    CONSTRAINT chk_unit_of_measurement CHECK (unit_of_measurement IN ('minutes', 'times', 'reps')),
     CONSTRAINT chk_completions_per_day CHECK (completions_per_day >= 1)
 );
 
