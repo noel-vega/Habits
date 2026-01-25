b194f9235dcfe42dbfd08f9abf8e4c816e365d1c
Noel Vega <noelvegajr94@gmail.com>
2026-01-25 18:12:50 -0500

    rm unused types

--- api/main.go
+++ api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,38 +11,18 @@ import (
 	"github.com/noel-vega/habits/api/internal/todos"
 )
 
-type PostgresRepository struct {
-	Habits        *habit.HabitRepo
-	Contributions *habit.ContributionsRepo
-	Todos         *todos.TodosRepo
-}
-
-func NewPostgresRepository(db *sqlx.DB) *PostgresRepository {
-	return &PostgresRepository{
-		Habits:        habit.NewHabitRepo(db),
-		Contributions: habit.NewContributionsRepo(db),
-		Todos:         todos.NewTodosRepo(db),
-	}
-}
-
 func main() {
 	db, err := sqlx.Connect("postgres", "user=habits dbname=habits password=habits sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	Init()
+
+	InitGoogleOAuth()
 
 	// Create a Gin router with default middleware (logger and recovery)
 	router := gin.Default()
 	router.Use(cors.Default())
 
-	// Define a simple GET endpoint
-	router.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
 	habit.AttachRoutes(router, db)
 	todos.AttachRoutes(router, db)
 
